37c7a68bb4a7c334178287d2a1f6ca7c46c5622d
Martijn Hiemstra <info@greenhighway.nl>
2022-11-30 10:11:23 +0100

    First commit

---
+++ src/test/java/nl/workingtalent/bookrental/BookrentalApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.workingtalent.bookrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookrentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
